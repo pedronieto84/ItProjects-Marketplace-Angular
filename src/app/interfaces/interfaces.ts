07bebda4b07d61c7126c8e835652d129c5a6bb9d
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-06-11 16:59:22 +0100

    I define the interfaces.

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,32 @@
+export interface User {
+
+    id: string
+    name: string
+    email: string
+    password: string
+    verified: boolean
+    publishedProjects: string[] // refers to the project id's
+
+}
+
+export interface Project {
+    id: string
+    title: string
+    ownerId: string
+    publishedDate: Date
+    deadlineDate: Date
+    techSet: string[] // refers to the TechObject Id's
+    filesArray: string[] // will be the url of the files
+    explanation: string
+    steate: ProjectState
+}
+
+export interface TechObject {
+    id: string
+    name: string
+}
+
+export interface ProjectState {
+    id: string
+    name: string
+}
