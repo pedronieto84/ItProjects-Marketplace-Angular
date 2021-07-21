43d1a53a2a72624196a1ea89798908bef489bd36
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-07-21 17:57:45 +0200

    Error NgbModule

--- src/app/pages/create-project/create-project.module.ts
+++ src/app/pages/create-project/create-project.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateProjectComponent } from './create-project.component';
 import { CreateProjectRoutingRoutingModule } from './create-project-routing.module';
+import { SharedModule } from 'src/app/components/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    CreateProjectComponent,
+    CreateProjectComponent
   ],
   imports: [
     CommonModule,
-    CreateProjectRoutingRoutingModule
+    CreateProjectRoutingRoutingModule,
+    SharedModule
+    
   ]
 })
 export class CreateProjectModule { }
