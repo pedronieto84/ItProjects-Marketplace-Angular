ad5ed283a57c859b23c152cf02259c4c06db61c7
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-07-26 18:47:55 +0200

    Registre fa validacions, falta disseny, routings i post al backend.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -12,6 +12,10 @@ export class RegisterComponent implements OnInit {
   password:string;
   name:string;
   typeOfInstitution:string;
+  hasEmail: boolean = false;
+  hasName: boolean = false;
+  hasPassword: boolean = false;
+  hasTypeOfInst: boolean = false;
 
   constructor() { }
 
@@ -33,13 +37,78 @@ export class RegisterComponent implements OnInit {
   get showName(){
     if(this.name !== undefined){
       if(this.name.length >= 2 && this.name.length <= 70){
+        this.hasName = true;
         return false
       }else{
+        this.hasName = false;
         return true
       }
     }else{
+      this.hasName = false;
       return false
     }
+  }
+
+  get showEmail(){
+       const patternEmail =
+        /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+   
+     if(this.email !== undefined){
+      if(this.email.match(patternEmail)){
+        this.hasEmail = true;
+        return false
+      }else{
+        this.hasEmail = false;
+        return true
+      }
+    }else{
+      this.hasEmail = false;
+      return false
+    }
+  }
+
+    get showPassword(){
+      
+       const patternPassword =
+         /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,10}$/
+   
+     if(this.password !== undefined){
+      if(this.password.match(patternPassword)){
+        this.hasPassword = true;
+        return false
+      }else{
+        this.hasPassword = false;
+        return true
+      }
+    }else{
+      this.hasPassword = false;
+      return false
+    }
+  }
+
+  get showTypeOfInstitution(){
+      if(this.typeOfInstitution !== undefined){
+      if(
+        this.typeOfInstitution === ('1') ||
+        this.typeOfInstitution === ('2') ||
+        this.typeOfInstitution === ('3') ||
+        this.typeOfInstitution === ('4') 
+        
+    ){
+        this.hasTypeOfInst = true;
+        return false
+      }else{
+        this.hasTypeOfInst = false;
+        return true
+      }
+    }else{
+      this.hasTypeOfInst = false;
+      return false
+    }
+  }
+
+  get isAllOk(){
+    return this.hasEmail && this.hasName && this.hasPassword && this.hasTypeOfInst
   }
 
 }
