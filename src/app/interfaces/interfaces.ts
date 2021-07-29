3ee85ac4908710f12f3ca872cf71dc80006a99f3
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-07-29 15:30:23 +0200

    He puesto cambios en la interface project para forzar un conflicto en interface project.

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -16,15 +16,15 @@ export interface User {
 
 export interface Project {
 
-    id: string
+    id: string // comentari 
     title: string
-    ownerId: string
-    publishedDate: Date
-    deadlineDate: Date
-    techSet: string[] // refers to the TechObject Id's
-    filesArray: string[] // will be the url of the files
-    explanation: string
-    steate: ProjectState
+    ownasdfasdfasfadsferId: string
+    asasdfasdfasdfdfasdf: Date
+    adsf: Date
+    asasdfasdfadsfdfasdf: string[] // refers to the TechObject Id's
+    asdadsfadsffasdf: string[] // will be the url of the files
+    adsfasf: string
+    asdfasd: ProjectState // he cambiado estate
 }
 
 export interface TechObject {
