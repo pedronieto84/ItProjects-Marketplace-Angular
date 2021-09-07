f41ab5dc292a634ff25fcd6e2cd29490fd268118
Patri <witty.free@gmail.com>
2021-09-07 16:16:32 +0200

    crear project-card componet

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DatePickerComponent } from './date-picker/date-picker.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    DatePickerComponent
+    DatePickerComponent,
+    ProjectCardComponent
   ],
   imports: [
     CommonModule,
