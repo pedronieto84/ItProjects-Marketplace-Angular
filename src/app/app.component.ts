14fca7912e3f358713e03acade0f23686b7af1da
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-09-15 18:14:57 +0200

    nuevo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'itProjects-Marketplace-Angular'; // fsdfafasd
+  title = 'itProjects-Marketplace-Angular'; // fsdfafasdfasdfadfasdfaf
 }
