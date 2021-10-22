3b5fdb05cd15b1fae518f5aeafe378effb96b1e3
David Castrillón <david.castri@hotmail.es>
2021-10-22 17:09:15 +0200

    Mockup table funcional según ejemplo Angular Material

--- src/app/components/feature-components/feature-components-module.module.ts
+++ src/app/components/feature-components/feature-components-module.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminTableComponent } from './admin-table/admin-table.component';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';   
 
 
 @NgModule({
@@ -10,7 +14,11 @@ import { MatSliderModule } from '@angular/material/slider';
   ],
   imports: [
     CommonModule,
-    MatSliderModule
+    MatSliderModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports : [
     AdminTableComponent
